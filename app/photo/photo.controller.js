98a9916f0f425e9469bc7fa23b4be17e2a58016e
Michael Ahari <michael.ahari@btinternet.com>
2016-10-02 01:13:33 +0100

    better mobile scaling

--- app/photo/photo.controller.js
+++ app/photo/photo.controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.
-module('myProfile.photo').controller('photoController', ['$scope', '$rootScope', function($scope, $rootScope){
+module('myProfile.photo').controller('photoController', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope){
 
+    // access_token=1993155397.ee0a964.245b5eecbda74e9baf201b1abff7e556
+
+
+    console.log(ENV['INSTAGRAM_TOKEN']);
+    $scope.getLatestPhoto = function(){
+
+        var token = ENV['INSTAGRAM_TOKEN']
+
+    }
 }]);
